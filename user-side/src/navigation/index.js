717de98d6d938e3931e6706152e5f1ce5f4cedb9
Piotr Janik <pj113610@stud.ur.edu.pl>
2022-09-29 18:18:13 +0200

    added login, profile, sign out logic

--- user-side/src/navigation/index.js
+++ user-side/src/navigation/index.js
@@ -1,11 +1,12 @@
 import { createNativeStackNavigator} from '@react-navigation/native-stack';
-import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
 import RestaurantDetailsPage from '../screens/RestaurantDetailsScreen';
 import DishDetailsScreen from '../screens/DishDetailsScreen';
 import Basket from '../screens/Basket';
 import OrderScreen from '../screens/OrdersScreen';
 import OrderDetails from '../screens/OrderDetails';
+import Profile from '../screens/ProfileScreen';
 import { Entypo } from '@expo/vector-icons'; 
 import { FontAwesome5 } from '@expo/vector-icons'; 
 import { FontAwesome } from '@expo/vector-icons'; 
@@ -20,14 +21,14 @@ const RootNavigator = () => {
     );
 };
 
-const Tab = createMaterialBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 
 const HomeTabs = () => {
     return(
-        <Tab.Navigator barStyle= {{backgroundColor: 'white'}}>
+        <Tab.Navigator screenOptions={{headerShown: false}} barStyle= {{backgroundColor: 'white'}}>
             <Tab.Screen name='Home' component={HomeStackNavigator} options={{tabBarIcon: ({color}) => <Entypo name="home" size={24} color={color} /> }}/>
             <Tab.Screen name='Orders' component={OrdersStackNavigator} options={{tabBarIcon: ({color}) => <FontAwesome5 name="clipboard-list" size={24} color={color} /> }}/>
-            <Tab.Screen name='Profile' component={OrderScreen} options={{tabBarIcon: ({color}) => <FontAwesome name="user" size={24} color={color} /> }}/>
+            <Tab.Screen name='Profile' component={Profile} options={{tabBarIcon: ({color}) => <FontAwesome name="user" size={24} color={color} /> }}/>
         </Tab.Navigator>
     );
 };
@@ -37,7 +38,7 @@ const HomeStackNavigator = () => {
     return(
         <HomeStack.Navigator>
         <HomeStack.Screen  name="Restaurants" component={HomeScreen} />
-        <HomeStack.Screen  name="Restaurant" component={RestaurantDetailsPage} />
+        <HomeStack.Screen  options={{headerShown: false}} name="Restaurant" component={RestaurantDetailsPage} />
         <HomeStack.Screen  name="Dish" component={DishDetailsScreen} />
         <HomeStack.Screen  name="Basket" component={Basket} />
         </HomeStack.Navigator>
